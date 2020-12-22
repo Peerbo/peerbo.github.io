021aebd30466f1d080f7b6a124758bd338b87cbc
Peerbo <kerim.rcpl@gmail.com>
2020-12-22 22:51:06 +0400

    Added the update bar :purple_heart:✨

--- js/main.js
+++ js/main.js
@@ -10,3 +10,19 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+/* Update Box */
+/* Shift everything to the left and open the sidebar */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "25vw";
+  document.getElementById("main").style.marginLeft = "25vw";
+  document.getElementById("headArticle").style.width = "72vw";
+  document.getElementById("boxArticle").style.width = "72vw";
+}
+/* Shift everything to the right and close the sidebar */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("headArticle").style.width = "98vw";
+  document.getElementById("boxArticle").style.width = "98vw";
+}
