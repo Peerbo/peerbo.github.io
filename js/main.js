816bd67a9433b3df74e46505214e755ba6597d8a
Peerbo <kerim.rcpl@gmail.com>
2020-12-24 11:34:04 +0400

    Dark Mode and Phone Fixes for Update Bar✨

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 /* Special Controls */
 
+
+
+
+    /* MIGHT NEED TO ADD NEW FUNCTION FOR PHONE SCREEN */
+
+
+
+
+
+
 /* Navigation Bar */
 /* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
 function myFunction() {
@@ -14,10 +24,19 @@ function myFunction() {
 /* Update Box */
 /* Shift everything to the left and open the sidebar */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "25vw";
-  document.getElementById("main").style.marginLeft = "25vw";
-  document.getElementById("headArticle").style.width = "72vw";
-  document.getElementById("boxArticle").style.width = "72vw";
+  var scrn = window.matchMedia("(max-width: 600px)");
+  if (scrn.matches) {
+    document.getElementById("mySidenav").style.width = "100vw";
+    document.getElementById("main").style.marginLeft = "100vw";
+    document.getElementById("headArticle").style.width = "0";
+    document.getElementById("boxArticle").style.width = "0";
+  }
+  else {
+    document.getElementById("mySidenav").style.width = "25vw";
+    document.getElementById("main").style.marginLeft = "25vw";
+    document.getElementById("headArticle").style.width = "72vw";
+    document.getElementById("boxArticle").style.width = "72vw";
+  }
 }
 /* Shift everything to the right and close the sidebar */
 function closeNav() {
